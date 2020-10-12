a711a786c22d52e08e8f7c1c9b94465e499c51b3
Clifton <clifton.baggerman@me.com>
2020-10-12 21:52:23 +1100

    added some safolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'material', loadChildren: () => import('./shared/material.module').then(m => m.MaterialModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
